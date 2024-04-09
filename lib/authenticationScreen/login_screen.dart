77e3e4a0d7dc13422fb82e42c373c01085dd0ad9
Sean Tighe <seanpatricktighe@gmail.com>
2024-04-09 16:13:59 +0900

    added the gradient styling, and create account button

--- lib/authenticationScreen/login_screen.dart
+++ lib/authenticationScreen/login_screen.dart
@@ -1,9 +1,10 @@
+// Import necessary packages
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:pawfect_match/authenticationScreen/registration_screen.dart';
 import 'package:pawfect_match/widgets/custom_text_field_widget.dart';
 
+// Define a StatefulWidget for the login screen
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -11,102 +12,100 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
+// Define the state for the LoginScreen widget
 class _LoginScreenState extends State<LoginScreen> {
+  // Define text editing controllers for the email and password fields
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+
+  // Define a boolean to control the visibility of a progress bar
   bool showProgressBar = false;
 
-
-
   @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold
-    (
-      body: SingleChildScrollView(
-
+  Widget build(BuildContext context) {
+    // Return a Scaffold widget to provide a visual structure to the screen
+    return Scaffold(
+        body: Container(
+      // Decorate the container with a linear gradient background
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.red, // Start color
+            Colors.pink, // Middle color
+            Colors.purple, // End color
+          ],
+          stops: [
+            0.1,
+            0.5,
+            1.2
+          ], // Adjust these stops for the distribution of colors
+        ),
+      ),
+      child: SingleChildScrollView(
         child: Center(
-
           child: Column(
-
             children: [
-
-              const SizedBox( 
-                height: 120,
-              ),
-             
+              // Add some space at the top
+              const SizedBox(height: 120),
+              // Display the logo image
               Image.asset(
                 'images/logo.png',
                 width: 180,
               ),
-
+              // Display the app title
               const Text(
-                "Pawfect match",
+                "Pawfect Match",
                 style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 232, 91, 91)
+                  color: Color.fromARGB(255, 255, 255, 255),
                 ),
               ),
-
-              const SizedBox( 
-                height: 30,
-              ),
-
+              // Add some space below the title
+              const SizedBox(height: 40),
+              // Display the login prompt
               const Text(
-                "Login now to find your Fur-ever mate",
+                "Login now and find your Fur-ever mate",
                 style: TextStyle(
                   fontSize: 18,
-                  color: Color.fromARGB(255, 232, 91, 91)
+                  color: Color.fromARGB(255, 255, 255, 255),
                 ),
               ),
-
-              const SizedBox( 
-                height: 30,
-              ),
-
+              // Add some space below the prompt
+              const SizedBox(height: 30),
+              // Display the email input field
               CustomTextFieldWidget(
-                //Email
                 editingController: emailTextEditingController,
                 lableText: "Email",
                 iconData: Icons.email_outlined,
                 isObscure: false,
               ),
-
-              const SizedBox( 
-                height: 15,
-              ),
-
+              // Add some space below the email field
+              const SizedBox(height: 15),
+              // Display the password input field
               CustomTextFieldWidget(
-                //Password
                 editingController: passwordTextEditingController,
                 lableText: "Password",
                 iconData: Icons.lock_outline,
                 isObscure: true,
               ),
-
-              const SizedBox( 
-                height: 15,
-              ),
-
+              // Add some space below the password field
+              const SizedBox(height: 55),
+              // Display the login button
               Container(
-
                 width: MediaQuery.of(context).size.width - 36,
                 height: 55,
                 decoration: const BoxDecoration(
-
                   color: Colors.white,
                   borderRadius: BorderRadius.all(
-                    Radius.circular(12)
-                  )
-                  
-
+                    Radius.circular(24),
+                  ),
                 ),
                 child: InkWell(
-
-                  onTap: ()
-                  {
-
+                  onTap: () {
+                    // Define the action to be taken when the login button is tapped
                   },
                   child: const Center(
                     child: Text(
@@ -115,64 +114,53 @@ class _LoginScreenState extends State<LoginScreen> {
                         fontSize: 20,
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
-
                       ),
                     ),
                   ),
-                )
+                ),
               ),
-
-              const SizedBox( 
-                height: 15,
-              ),
-
-              //Button to Register a new account 
+              // Add some space below the login button
+              const SizedBox(height: 15),
+              // Display the registration prompt and button
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-
-                  Text(
-                  
+                  const Text(
                     "Don't have an account yet? ",
                     style: TextStyle(
                       fontSize: 16,
-                      color: Colors.grey,
+                      color: Color.fromARGB(255, 0, 0, 0),
                     ),
                   ),
-
                   InkWell(
-                    onTap: ()
-                    {
+                    onTap: () {
+                      // Navigate to the registration screen when the registration button is tapped
                       Get.to(RegistraionScreen());
                     },
                     child: const Text(
-                      "Create here",
+                      "Create one here",
                       style: TextStyle(
                         fontSize: 18,
-                        color: Colors.white,
+                        color: Color.fromARGB(255, 0, 162, 255),
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )
+                  ),
                 ],
               ),
-              const SizedBox( 
-                height: 15,
-              ),
-
-              //Loading bar
-              showProgressBar == true ? CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.pink),
-              ) : Container(),
-
-              const SizedBox( 
-                height: 15,
-              ),
-              
+              // Add some space below the registration button
+              const SizedBox(height: 15),
+              // Display a progress bar if showProgressBar is true
+              if (showProgressBar)
+                const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.pink),
+                ),
+              // Add some space at the bottom
+              const SizedBox(height: 15),
             ],
           ),
         ),
       ),
-    );
+    ));
   }
 }
